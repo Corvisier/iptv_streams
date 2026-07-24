d8a685721091e6bf8b8e1af859d7aa17fee72fa4
Hélder Teixeira <corvisierpt@gmail.com>
2026-07-25 00:09:36 +0100

    Update

--- EPG/epg-pt.sh
+++ EPG/epg-pt.sh
@@ -2,35 +2,69 @@
 
 cd /home/runner/work/iptv_streams/iptv_streams/iptv-org-epg && npm install
 
-# PT EPG
+# IPTVSTREAMS EPG
 
-npm run grab -- --channels=../EPG/pt.channels.xml --output=../EPG/epg-pt.xml --days=7 --maxConnections=10
-
-# Meo EPG
-
-npm run grab -- --site=meo.pt --output=../EPG/epg-meo-pt.xml --days=7 --maxConnections=2
-
-# Nos EPG
-
-npm run grab -- --site=nostv.pt --output=../EPG/epg-nos-pt.xml --days=7 --maxConnections=10
+npm run grab --- --channels=../EPG/iptv_streams.channels.xml --output=../EPG/epg-iptv-streams.xml --days=7 --maxConnections=20
 
 # RTP EPG
 
-npm run grab -- --site=rtp.pt --output=../EPG/epg-rtp-pt.xml --days=7 --maxConnections=5
+npm run grab --- --sites=rtp.pt --output=../EPG/epg-rtp-pt.xml --days=7 --maxConnections=20
+
+# SIC EPG
+
+npm run grab --- --sites=opto.sic.pt --output=../EPG/epg-sic-pt.xml --days=7 --maxConnections=20
+
+# TVI EPG
+
+npm run grab --- --sites=tvi.iol.pt --output=../EPG/epg-tvi-pt.xml --days=7 --maxConnections=20
+
+# SportTV EPG
+
+npm run grab --- --sites=sporttv.pt --output=../EPG/epg-sporttv-pt.xml --days=7 --maxConnections=20
+
+# Vodafone PT EPG
+
+npm run grab --- --sites=vodafone.pt --output=../EPG/epg-vodafone-pt.xml --days=7 --maxConnections=20
+
+# Nos EPG
+
+npm run grab --- --sites=nostv.pt --output=../EPG/epg-nos-pt.xml --days=7 --maxConnections=20
+
+# Meo EPG
+
+npm run grab --- --sites=meo.pt --output=../EPG/epg-meo-pt.xml --days=7 --maxConnections=20
 
 # Mi.tv EPG
 
-npm run grab -- --channels=sites/mi.tv/mi.tv_br.channels.xml --output=../EPG/epg-mitv-br.xml --days=7 --maxConnections=10
+npm run grab --- --channels=sites/mi.tv/mi.tv_br.channels.xml --output=../EPG/epg-mitv-br.xml --days=7 --maxConnections=20
+
+# Vivo Play EPG
+
+npm run grab --- --sites=vivoplay.com.br --output=../EPG/epg-vivoplay-br.xml --days=7 --maxConnections=20
+
+# Sky IT EPG
+
+npm run grab --- --sites=guidatv.sky.it --output=../EPG/epg-sky-it.xml --days=7 --maxConnections=20
+
+# Sky UK EPG
+
+npm run grab --- --sites=sky.com --output=../EPG/epg-sky-uk.xml --days=7 --maxConnections=20
+
+# Freeview Play EPG
+
+npm run grab --- --sites=freeview.co.uk --output=../EPG/epg-freeview-uk.xml --days=7 --maxConnections=20
+
+# Sky DE EPG
+
+npm run grab --- --sites=sky.de --output=../EPG/epg-sky-de.xml --days=7 --maxConnections=20
 
 # Rytec EPG
 
-cd ../EPG
-
-wget -O epg-rytec-pt.xml.xz "http://www.xmltvepg.nl/rytecPT.xz"
+cd ../EPG && wget -O epg-rytec-pt.xml.xz "http://www.xmltvepg.nl/rytecPT.xz"
 
 # Compress EPG xml files
 
-xz -k -f -9 epg*.xml && gzip -k -f -9 epg*.xml
+xz -k -f -9 epg*.xml && gzip -k -f -9 epg-iptv-streams.xml
 
 # Remove EPG xml files
 
--- M3U/streams.sh
+++ M3U/streams.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
 
-# Método 1
+# Method 1
 
-# TVI - Actualiza a URL da TVI
+# Update the stream URL of all TVI channels
 
-sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" iptv_streams.m3u
+TOKEN=$(wget -qO- --timeout=15 --tries=2 https://services.iol.pt/matrix?userId)
 
-# CNN Portugal - Actualiza a URL da CNN Portugal
+# If the token comes back empty (endpoint down), keep the existing valid token
+# instead of blanking it across all files.
+if [ -z "$TOKEN" ]; then
+  echo "Empty token from IOL endpoint — keeping existing tokens."
+  exit 0
+fi
 
-sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" iptv_streams.m3u
-
-# TVI Internacional - Actualiza a URL da TVI Internacional
-
-sed -i "/live_tvi_internacional/ c https://video-auth6.iol.pt/live_tvi_internacional/live_tvi_internacional/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" iptv_streams.m3u
-
-# Método 2
-
-# TVI - Actualiza a URL da TVI
-
-sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "TVI.m3u"
-
-# TVI Internacional - Actualiza a URL da TVI Internacional
-
-sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "TVI_Internacional.m3u"
-
-# CNN Portugal - Actualiza a URL da CNN Portugal
-
-sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "CNN_Portugal.m3u"
+sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=${TOKEN}#g" *.m3u8
 
 exit 0
